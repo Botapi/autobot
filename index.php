fe134da7db0dd3039cc3e5af46ad2f76e2497c23
Botapi5 <Botapi58@gmail.com>
2018-11-27 16:40:33 +0700

    Initial...

--- index.php
+++ index.php
@@ -23,6 +23,21 @@ if (!is_null($events['events'])) {
 
             // Get replyToken
             $replyToken = $event['replyToken'];
+
+            // Location
+            $title = 'ตำแหน่งที่ตั้ง';
+            $address = 'คณะวิทยาศาตร์และเทคโนโลยี';
+            $latitude = '7.9097011';
+            $longitude = '98.3847784';
+           
+             $httpClient = new CurlHTTPClient($channel_token);
+             $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+             
+             
+             $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);
+             $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+             
             $ask = $event['message']['text'];
             switch(strtolower($ask)) {
                 case 'สวัสดี' :
@@ -57,21 +72,9 @@ if (!is_null($events['events'])) {
                 case 'ติดต่อ':
                     $respMessage = 'เลขที่ 21 หมู่ที่ 6 ตำบลรัษฎา อำเภอเมือง จังหวัดภูเก็ต 83000 หมายเลขโทรศัพท์ 076-240-474 ต่อ 4000, 076-211-959 ต่อ 4000 หมายเลขโทรศัพท์ / โทรสาร 076-218-80';
                     break;
-               // Location
-               $title = 'ตำแหน่งที่ตั้ง';
-               $address = 'คณะวิทยาศาตร์และเทคโนโลยี';
-               $latitude = '7.9097011';
-               $longitude = '98.3847784';
-              
-                $httpClient = new CurlHTTPClient($channel_token);
-                $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-                
-                
-                $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);
-                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-                //default:
-                    //$respMessage = 'ขอโทษนะค่ะคำถามนี้ไม่เกี่ยวข้องกับคณะวิทยาศาสตร์และเทคโนโลยีค่ะ';
-                   // break;
+                default:
+                    $respMessage = 'ขอโทษนะค่ะคำถามนี้ไม่เกี่ยวข้องกับคณะวิทยาศาสตร์และเทคโนโลยีค่ะ';
+                    break;
                    
             }
             $httpClient = new CurlHTTPClient($channel_token);
