b18fdec323ff469ad037493ba36e8262ec0e8f2b
Botapi5 <Botapi58@gmail.com>
2018-11-27 16:18:27 +0700

    Initial...

--- index.php
+++ index.php
@@ -63,16 +63,17 @@ if (!is_null($events['events'])) {
                    
                     // Location
                 //$title = 'ตำแหน่งที่ตั้ง';
-                $case 'ตำแหน่งที่ตั้ง':
+                $title = $event['message']['text'];
+                case 'ตำแหน่งที่ตั้ง':
                 $address = 'คณะวิทยาศาตร์และเทคโนโลยี';
                 $latitude = '7.9097011';
                 $longitude = '98.3847784';
-            
+               
             $httpClient = new CurlHTTPClient($channel_token);
             $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
             
             
-            $textMessageBuilder = new LocationMessageBuilder($case,$title, $address, $latitude, $longitude);
+            $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);
             $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                     
                 
