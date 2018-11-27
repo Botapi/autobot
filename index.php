b7da19f65574719ef9add5acc2069d8d448c5216
Botapi5 <Botapi58@gmail.com>
2018-11-27 16:09:51 +0700

    Initial...

--- index.php
+++ index.php
@@ -62,11 +62,12 @@ if (!is_null($events['events'])) {
                     break;
                    
                     // Location
-            $title = 'ตำแหน่งที่ตั้ง';
-            $address = 'คณะวิทยาศาตร์และเทคโนโลยี';
-            $latitude = '7.9097011';
-            $longitude = '98.3847784';
-            
+                case 'ตำแหน่งที่ตั้ง':
+                //$title = 'ตำแหน่งที่ตั้ง';
+                $address = 'คณะวิทยาศาตร์และเทคโนโลยี';
+                $latitude = '7.9097011';
+                $longitude = '98.3847784';
+                    break;
             
             $httpClient = new CurlHTTPClient($channel_token);
             $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
