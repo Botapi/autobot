27a9e1fd3d672f41fe784ed34f17a715fd4d37c2
Botapi5 <Botapi58@gmail.com>
2018-12-02 22:38:30 +0700

    Initial...

--- index.php
+++ index.php
@@ -33,8 +33,8 @@ $database = $firebase->getDatabase();
 $newPost = $database
     ->getReference('blog/posts')
     ->push([
-        'title' => 'Post title',
-        'body' => 'This should probably be longer.'
+        'title' => 'พับจี',
+        'body' => 'This should'
     ]);
 $newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
 $newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
